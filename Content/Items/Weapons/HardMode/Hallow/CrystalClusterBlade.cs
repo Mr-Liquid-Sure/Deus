0bca9f6d8f4e0fe6fa3539038d771b517f79a84b
Mr-Liquid-Sure <Liquidatemod@gmail.com>
2023-11-07 17:14:46 +0000

    Xinim I couldnt do the pull thing

--- Content/Items/Weapons/HardMode/Hallow/CrystalClusterBlade.cs
+++ Content/Items/Weapons/HardMode/Hallow/CrystalClusterBlade.cs
@@ -10,24 +10,20 @@ namespace Deus.Content.Items.Weapons.HardMode.Hallow
 {
     public class CrystalClusterBlade : ModItem
     {
-      
         public override void SetStaticDefaults()
         {
-            
-
             CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
-
         }
 
         public override void SetDefaults()
         {
             Item.damage = 14;
             Item.DamageType = DamageClass.Melee;
-            Item.autoReuse = true;
+            Item.autoReuse = false;
             Item.width = 32;
             Item.height = 36;
-            Item.useTime = 104;
-            Item.useAnimation = 104;
+            Item.useTime = 43;
+            Item.useAnimation = 43;
             Item.useStyle = ItemUseStyleID.Swing;
             Item.knockBack = 3f;
             Item.value = Item.buyPrice(0, 0, 87, 34);
@@ -35,27 +31,16 @@ namespace Deus.Content.Items.Weapons.HardMode.Hallow
             Item.crit = 2;
             Item.UseSound = SoundID.Item1;
             Item.shoot = ModContent.ProjectileType<CrystalClusterProj>();
+            Item.noUseGraphic = true;
         }
 
-        public override Vector2? HoldoutOffset()
-        {
-            var offset = new Vector2(6, 0);
-            return offset;
-        }
         public override void MeleeEffects(Player player, Rectangle hitbox)
         {
-            Vector2 speed = Main.rand.NextVector2CircularEdge(2f, 2f);
-            var d = Dust.NewDustPerfect(Item.Center, DustID.RainbowTorch, speed * 5, Scale: 3f);
-            ;
-            d.noGravity = true;
+
         }
         public override void AddRecipes()
         {
             Recipe recipe = CreateRecipe();
-
-            
-           
-
             recipe.AddTile(TileID.Anvils);
             recipe.Register();
         }
--- Content/Projectiles/HallowProj/CrystalClusterProj.cs
+++ Content/Projectiles/HallowProj/CrystalClusterProj.cs
@@ -11,71 +11,52 @@ namespace Deus.Content.Projectiles.HallowProj
     {
         public override void SetStaticDefaults()
         {
-            
             Main.projFrames[Projectile.type] = 13;
-
             ProjectileID.Sets.CultistIsResistantTo[Projectile.type] = true;
         }
 
         public override void SetDefaults()
         {
-            Projectile.width = 148;
-            Projectile.height = 262;
+            Projectile.width = 150;
+            Projectile.height = 275;
 
             Projectile.DamageType = DamageClass.Melee;
             Projectile.friendly = true;
             Projectile.hostile = false;
             Projectile.ignoreWater = true;
             Projectile.tileCollide = false;
-            Projectile.timeLeft = 104;
+            Projectile.timeLeft = 999;
             Projectile.light = 1;
             Projectile.penetrate = -1;
-            Projectile.extraUpdates = 1;
-            Projectile.scale = 1.5f;
         }
-
+        Vector2 mouseFirstPos = Vector2.Zero;
         public override void AI()
         {
-            Player player = Main.player[Projectile.owner];
 
-            if (++Projectile.frameCounter >= 8f)
+
+
+            if (Projectile.timeLeft == 999)
+            {
+                mouseFirstPos = Main.MouseWorld;
+            }
+            CheckFrame();
+            Player player = Main.player[Projectile.owner];
+            Projectile.spriteDirection = (player.Center.X - mouseFirstPos.X) > 0 ? -1 : 1;
+            Projectile.Center = player.Center - Vector2.UnitX.RotatedBy((player.Center - mouseFirstPos).ToRotation()) * 60;
+            float rotation = Projectile.spriteDirection == 1 ? MathHelper.Pi : 0;
+            Projectile.rotation = (player.Center - mouseFirstPos).ToRotation() + rotation;
+        }
+        private void CheckFrame()
+        {
+            if (++Projectile.frameCounter >= 4f)
             {
                 Projectile.frameCounter = 0;
-
                 if (++Projectile.frame >= Main.projFrames[Projectile.type])
+                {
                     Projectile.frame = 0;
+                    Projectile.Kill();
+                }
             }
-            var swingangle = new Vector2(Projectile.ai[0], Projectile.ai[1]);
-            Projectile.rotation = swingangle.ToRotation();
-            Projectile.position = player.Center + swingangle - new Vector2(Projectile.width / 2, Projectile.height / 2);
-            Projectile.spriteDirection = Projectile.direction;
-
-
-        }
-
-        public override bool PreDraw(ref Color lightColor)
-        {
-            Vector2 drawPosition = Projectile.Center;
-            Color color = Color.Cyan;
-            _ = Lighting.GetColor((int)drawPosition.X / 16, (int)(drawPosition.Y / 16f));
-            SpriteEffects spriteEffects = SpriteEffects.None;
-            if (Projectile.spriteDirection == -1)
-                spriteEffects = SpriteEffects.FlipHorizontally;
-
-            var texture = (Texture2D)ModContent.Request<Texture2D>(Texture);
-
-            int frameHeight = texture.Height / Main.projFrames[Projectile.type];
-            int startY = frameHeight * Projectile.frame;
-
-            var sourceRectangle = new Rectangle(0, startY, texture.Width, frameHeight);
-
-            Vector2 origin = sourceRectangle.Size() / 2f;
-            Color drawColor = Projectile.GetAlpha(lightColor);
-            Main.EntitySpriteDraw(texture,
-                Projectile.Center - Main.screenPosition + new Vector2(0f, Projectile.gfxOffY),
-                sourceRectangle, drawColor, Projectile.rotation, origin, Projectile.scale, spriteEffects, 0);
-            
-            return false;
         }
     }
 }

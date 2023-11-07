47eff168ba241a768b57ff679cf617c33b57efb4
Xinim <LELwhyyoulookingatthis@gmail.com>
2023-11-07 22:56:27 +0700

    don't ask me tf is this okay

--- Content/Items/Weapons/HardMode/Hallow/CrystalClusterBlade.cs
+++ Content/Items/Weapons/HardMode/Hallow/CrystalClusterBlade.cs
@@ -1,22 +1,17 @@
-
-using Deus.Content.Projectiles.HallowProj;
-using Microsoft.Xna.Framework;
 using Terraria;
-using Terraria.GameContent.Creative;
 using Terraria.ID;
 using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+using Terraria.GameContent.Creative;
+using Deus.Content.Projectiles.HallowProj;
 
 namespace Deus.Content.Items.Weapons.HardMode.Hallow
 {
     public class CrystalClusterBlade : ModItem
     {
-      
         public override void SetStaticDefaults()
         {
-            
-
             CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
-
         }
 
         public override void SetDefaults()
@@ -26,8 +21,8 @@ namespace Deus.Content.Items.Weapons.HardMode.Hallow
             Item.autoReuse = true;
             Item.width = 32;
             Item.height = 36;
-            Item.useTime = 104;
-            Item.useAnimation = 104;
+            Item.useTime = 20;
+            Item.useAnimation = 20;
             Item.useStyle = ItemUseStyleID.Swing;
             Item.knockBack = 3f;
             Item.value = Item.buyPrice(0, 0, 87, 34);
@@ -35,13 +30,9 @@ namespace Deus.Content.Items.Weapons.HardMode.Hallow
             Item.crit = 2;
             Item.UseSound = SoundID.Item1;
             Item.shoot = ModContent.ProjectileType<CrystalClusterProj>();
+            Item.noUseGraphic = true;
         }
 
-        public override Vector2? HoldoutOffset()
-        {
-            var offset = new Vector2(6, 0);
-            return offset;
-        }
         public override void MeleeEffects(Player player, Rectangle hitbox)
         {
             Vector2 speed = Main.rand.NextVector2CircularEdge(2f, 2f);
@@ -52,10 +43,6 @@ namespace Deus.Content.Items.Weapons.HardMode.Hallow
         public override void AddRecipes()
         {
             Recipe recipe = CreateRecipe();
-
-            
-           
-
             recipe.AddTile(TileID.Anvils);
             recipe.Register();
         }

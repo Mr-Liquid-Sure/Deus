e6df5d9b1046b7e58217bf5d73d80973b6ebb286
AnonZero960 <NullZero1@outlook.com>
2023-11-27 23:00:07 +0200

    tile merge. check common/utils

---
+++ Common/Utils/DeusUtils.Tile.cs
@@ -0,0 +1,18 @@
+﻿using Terraria;
+
+namespace Deus.Common.Utils;
+
+public partial class DeusUtils
+{
+    //To use this,,,
+    //In a tile's SetStaticDefaults....
+    //(Mod Solution name).tileMerge[Type, Mod.Find<ModTile>("{TILE_NAME}").Type] = true; (Without the curly brackets)
+    //Deus.tileMerge[Type, Mod.Find<ModTile>("{TILE_NAME}").Type] = true;
+    //Repeat this if you want specific modded blocks to merge together.
+    // - Zero
+    public static void Merge(int tile, int tile2)
+    {
+        Main.tileMerge[tile][tile2] = true;
+        Main.tileMerge[tile2][tile] = true;
+    }
+}
--- Deus.cs
+++ Deus.cs
@@ -1,9 +1,26 @@
+using Terraria;
 using Terraria.ModLoader;
+using Deus.Common.Utils;
 
 namespace Deus
 {
 	public class Deus : Mod
     {
-		
+        //click the lil thing on the left side to expand and collapse the region :heart:
+        #region Tile Merge Stuff - Zero
+        //Tile Merge Stuff
+        // - Zero
+        private static TileTest v = new();
+        public static TileTest tileMerge => v;
+
+        public class TileTest
+        {
+            public bool this[int tile1, int tile2]
+            {
+                get => Main.tileMerge[tile1][tile2] || Main.tileMerge[tile2][tile1];
+                set => DeusUtils.Merge(tile1, tile2);
+            }
+        }
+        #endregion
     }
 }
